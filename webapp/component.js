35bc40ca79a79967fc009bedd77cf80633869a32
Nguyen Ngoc Anh <anhnn@domain.vnext.vn>
2024-04-09 13:40:54 +0700

    Step 2+3+4

---
+++ webapp/component.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/m/Text"
+], function(Text) {
+    "use strict";
+
+    /* Step 2: Bootstrap
+    // alert("UI5 xin chào Bùi Trung Hiếu");
+    */
+
+    //  Step 3: Controls
+    new Text({
+        text:"Hello Bui Trung Hieu xinh gái!"
+    }).placeAt("content");
+    
+    
+});
